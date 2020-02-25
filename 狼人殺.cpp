e4c1f8636fc15f6b52357e73045afdb5aa0a277d
Avrill <A8206115@g.pccu.edu.tw>
2020-02-25 09:14:08 +0800

    two days, can't do the judge mode

--- 狼人殺.cpp
+++ 狼人殺.cpp
@@ -7,11 +7,11 @@ int main(){
 	srand(time(0));
 	
 	int choice,position[9]={},player[9]={1,2,3,4,5,6,7,8,9},start,player_out,choice_witch;
-	int poison; 
+	int poison,nothing; 
 	
 	cout<<"歡迎進入狼人殺，請挑選遊戲模式："<<endl;
-	cout<<"三神三狼三民（女巫、獵人、預言家 / 小狼 / 平民）請打 1 "<<endl;
-	cout<<"四神三狼三民（女巫、獵人、騎士、預言家 / 狼王、小狼 / 平民）請打 2 "<<endl;
+	cout<<"三神三狼三民（女巫、獵人、預言家 / 小狼 / 平民）進入遊戲請打 1 "<<endl;
+	//cout<<"四神三狼三民（女巫、獵人、騎士、預言家 / 狼王、小狼 / 平民）請打 2 "<<endl;
 	
 	cin>>choice;	//選擇模式 
 	system("CLS");
@@ -42,8 +42,8 @@ int main(){
 			system("CLS");
 		}//fori	
 		
-		//////////////////////////////////////
-		cout<<"天黑請閉眼"<<endl; //閉眼時間 
+		////////////////////////////////////// 第一晚 
+		cout<<"天黑請閉眼"<<endl;  
 		system("pause");
 		system("CLS");
 		cout<<"狼人請睜眼，請選擇要刀的玩家：";
@@ -53,6 +53,7 @@ int main(){
 		cout<<"女巫請睜眼，被刀的玩家是你要救他嗎？(救他請打1，不救請打2)：";
 		cin>>choice_witch;
 		if(choice_witch==1){   //女巫的選擇 
+			cout<<"請問你要毒人嗎？要毒的玩家是？(只能使用一瓶藥)：";
 			system("pause");
 			system("CLS");
 			cout<<"昨晚是平安夜"<<endl;
@@ -65,33 +66,122 @@ int main(){
 				player[poison-1]=0;
 			}
 			system("CLS");
+			if(poison!=0) cout<<"昨晚是"<<player_out<<"號及"<<poison<<"號玩家死亡"<<endl; 			
+			else cout<<"昨晚是"<<player_out<<"號玩家死亡"<<endl; 			
 		}
 		
 		//////////////////////////////////////
-		
-		
-		start=rand()%9+1;
-		if(poison!=0) cout<<"昨晚是"<<player_out<<"號及"<<poison<<"號玩家死亡"<<endl; 
-		else cout<<"昨晚是"<<player_out<<"號玩家死亡"<<endl; 
-		
-		cout<<"抽籤，由玩家"<<start<<"號玩家開始發言"<<endl; 
+				
+		start=rand()%9;
+	
+		cout<<"抽籤，由玩家"<<start+1<<"號玩家開始發言"<<endl; 
 		system("pause");
 		system("CLS");
 		
 		for(int i=0; i<9; i++){  //發言時間 
-			if(start+i>9) start-=9;
-			if(player[i]==0) continue;
-			cout<<"玩家"<<player[start+i-1]<<"的發言時間"<<endl<<"結束"; 
+			if(start+i>8) start-=9;
+			if(player[start+i]==0) continue;		
+			cout<<"玩家"<<player[start+i]<<"的發言時間"<<endl<<"結束"; 
 			system("pause");
 			system("CLS");
 		}
 		
 		cout<<"開始投票，請選擇投票出去的人：";
 		cin>>player_out;
+		player[player_out-1]=0;
+		
+					
+		////////////////////////////////////// 第二晚 
+		cout<<"天黑請閉眼"<<endl;  
+		system("pause");
+		system("CLS");
+		cout<<"狼人請睜眼，請選擇要刀的玩家：";
+		cin>>player_out;
+		system("CLS");
+		
+		if(choice_witch==1){ //使用解藥 未使用毒藥 
+			//cout<<"使用解藥 未使用毒藥 "<<endl;
+			cout<<"女巫請睜眼，被刀的玩家是你要救他嗎？(你已沒有解藥)：";
+			system("pause");
+			cout<<"請問你要毒人嗎？要毒的玩家是？(不毒請打0，毒人請選玩家號碼)：";
+			cin>>poison;
+			player[player_out-1]=0;
+			if(poison!=0){
+				player[poison-1]=0;
+				cout<<"昨晚是"<<player_out<<"號及"<<poison<<"號玩家死亡"<<endl; 			
+			}//if poison		
+			else cout<<"昨晚是"<<player_out<<"號玩家死亡"<<endl;  
+		}//使用解藥 未使用毒藥 
+		
+		if(choice_witch==2 && poison!=0){ //未使用解藥 已使用毒藥 
+			//cout<<"未使用解藥 已使用毒藥 "<<endl;
+			cout<<"女巫請睜眼，被刀的玩家是你要救他嗎？(救他請打1，不救請打2)：";
+			cin>>choice_witch;
+			if(choice_witch==1){   //女巫的選擇 
+				cout<<"請問你要毒人嗎？要毒的玩家是？(只能使用一瓶藥)：";
+				system("pause");
+				system("CLS");
+				cout<<"昨晚是平安夜"<<endl;
+			}
+			else if(choice_witch==2){			
+				player[player_out-1]=0;
+				cout<<"請問你要毒人嗎？要毒的玩家是？(已使用毒藥)：";
+				system("pause");
+				system("CLS"); 
+			}	
+		} //未使用解藥 已使用毒藥 		
+		
+		if(choice_witch==2 && poison==0){ //未使用解藥 未使用毒藥 
+			//cout<<"未使用解藥 未使用毒藥  "<<endl;
+			cout<<"女巫請睜眼，被刀的玩家是你要救他嗎？(救他請打1，不救請打2)：";
+			cin>>choice_witch;
+			if(choice_witch==1){   //女巫的選擇 
+				cout<<"請問你要毒人嗎？要毒的玩家是？(只能使用一瓶藥)：";
+				system("pause");
+				system("CLS");
+				cout<<"昨晚是平安夜"<<endl;
+			}
+			else if(choice_witch==2){ 
+				player[player_out-1]=0;
+				cout<<"請問你要毒人嗎？要毒的玩家是？(不毒請打0，毒人請選玩家號碼)：";
+				cin>>poison;
+				if(poison!=0){
+					player[poison-1]=0;
+				}
+				system("CLS");
+				if(poison!=0) cout<<"昨晚是"<<player_out<<"號及"<<poison<<"號玩家死亡"<<endl; 			
+				else cout<<"昨晚是"<<player_out<<"號玩家死亡"<<endl; 			
+			}		
+		}//未使用解藥 未使用毒藥 
+		
+		start=rand()%9;
+	
+		cout<<"抽籤，由玩家"<<start+1<<"號玩家開始發言"<<endl; 
+		system("pause");
+		system("CLS");
+		
+		for(int i=0; i<9; i++){  //發言時間 
+			if(start+i>8) start-=9;
+			if(player[start+i]==0) continue;		
+			cout<<"玩家"<<player[start+i]<<"的發言時間"<<endl<<"結束"; 
+			system("pause");
+			system("CLS");
+		}
+		
+		cout<<"開始投票，請選擇投票出去的人：";
+		cin>>player_out;
+		player[player_out-1]=0;
+		
+		
+		
+//		for(int i=0; i<9; i++){
+//			cout<<player[i];
+//		}
+		
 		
 	}//if outside 
-	
-	
 }
+
+
 
 
